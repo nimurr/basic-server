fe7df7803a194e70c3b203deafc9c8e1e952dc81
nimurr <nimurnerob404@gmail.com>
2025-01-08 15:38:50 +0600

    upsate

--- src/app.js
+++ src/app.js
@@ -1,17 +1,47 @@
-import express  from 'express';
-import cors from 'cors'
-import userRouter from './routes/user.routes.js';  
-
+import express from 'express';
+import cors from 'cors';
+import userRouter from './routes/user.routes.js';
 
 export const app = express();
 
 // Middleware to parse JSON data
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
- 
-
-app.use('/api/v1' , userRouter) 
-
+//=================== Routes =================
+app.use('/api/v1', userRouter);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=================== API error handller  ================
+
+// Custom 404 Middleware for Routes Not Found
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: 'API endpoint not found',
+    });
+});
+
+// Centralized Error-Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack for debugging
+
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || 'Internal Server Error',
+    });
+});
